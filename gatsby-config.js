8c8f0c72576781c59416beaff21b116adafa8e31
Ilya Boyandin <ilyabo@gmail.com>
2018-09-08 20:11:26 +0200

    Attempting to add pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,9 +5,45 @@ module.exports = {
     title: 'Ilya Boyandin',
   },
   plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: 'gatsby-remark-responsive-iframe',
+            options: {
+              wrapperStyle: 'margin-bottom: 1.0725rem',
+            },
+          },
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
+        ],
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        //trackingId: 'ADD YOUR TRACKING ID HERE',
+      },
+    },
     'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
         name: 'Ilya Boyandin',
         short_name: 'Ilya Boyandin',
