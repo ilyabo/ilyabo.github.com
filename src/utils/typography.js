44c25359f97d10e3ad47a0fb14d82275779defbe
Ilya Boyandin <ilyabo@gmail.com>
2018-09-08 20:43:32 +0200

    Using moraga theme for typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,24 +1,7 @@
 import Typography from 'typography'
+import moragaTheme from 'typography-theme-moraga'
 
-// import Wordpress2016 from 'typography-theme-wordpress-2016'
-// Wordpress2016.overrideThemeStyles = () => ({
-//   'a.gatsby-resp-image-link': {
-//     boxShadow: 'none',
-//   },
-// })
-//
-// delete Wordpress2016.googleFonts
-//
-// const typography = new Typography(Wordpress2016)
-
-
-const typography = new Typography({
-  baseFontSize: '18px',
-  baseLineHeight: 1.45,
-  headerFontFamily: ['Lato', 'Helvetica Neue', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif'],
-  bodyFontFamily: ['Lato', 'Helvetica', 'Arial', 'sans-serif'],
-})
-
+const typography = new Typography(moragaTheme)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== 'production') {
