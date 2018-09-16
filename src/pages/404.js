b76ae340290e1c5508ae6eee94d76dd98ca6a1e5
Ilya Boyandin <ilyabo@gmail.com>
2018-09-16 23:44:27 +0200

    Better 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link } from 'gatsby'
+import { rhythm } from '../utils/typography'
 
 const NotFoundPage = () => (
-  <div>
+  <div style={{
+    padding: rhythm(1)
+  }}>
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Link to="/">Just visit the main page instead!</Link>
   </div>
 )
 
