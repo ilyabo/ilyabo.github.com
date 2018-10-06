546bf7a59187a9ef3e5d2c9f1b368723fd21eb7f
Ilya Boyandin <ilyabo@gmail.com>
2018-10-06 23:56:43 +0200

    Improved header menu coloring

--- src/components/Header.js
+++ src/components/Header.js
@@ -36,26 +36,45 @@ import { rhythm } from '../utils/typography'
 
 
 const Outer = styled('div')`
+  // box-shadow: 0px 0px 10px ${colors.primary[1]};
   background-color: ${colors.primary[1]};
   transform: translate3D(0,0,0);
   width: 100%;
   z-index: 2;
-  padding: ${rhythm(0.3)} ${rhythm(1)};
+  padding: 0 ${rhythm(0)};
   display: flex;
   align-items: center;
   .filler {
     flex-grow: 1;
   }
-  * + * {
-    margin-left: 2em;
-  }
   a {
+    text-decoration: none;
     display: flex;
     color: #fff;
+    padding: ${rhythm(0.3)} ${rhythm(1)};
     font-size: 11pt;
+    cursor: pointer;
+    transition: color 0.25s, background-color 0.25s; 
+    &:hover {
+      background-color: #ffffff80;
+      color: ${colors.primary[0]} !important;
+    }
   }
 `
 
+const activeLinkCss = css(`
+  background-color: #ffffffb0 !important;
+  color: ${colors.primary[0]} !important;
+`)
+
+
+const SocialLinksArea = styled('div')`
+  display: flex;
+  margin-right: ${rhythm(0.7)}; 
+  a {
+    padding: ${rhythm(0.3)} ${rhythm(0.3)} !important;
+  }  
+`
 
 const Header = ({ data }) =>
   <Headroom
@@ -70,18 +89,15 @@ const Header = ({ data }) =>
   >
     <Outer>
       {/*<Logo/>*/}
-      <Link to="/">Home</Link>
-      <Link to="/about/">About</Link>
+      <Link to="/" activeClassName={activeLinkCss}>Home</Link>
+      <Link to="/about/" activeClassName={activeLinkCss}>About</Link>
       <a href="/resume/" target="_blank" rel="noopener">CV</a>
       <div className="filler"/>
-      <div className={css({
-        marginLeft: 7,
-        display: 'flex',
-      })}>
-        <a href="https://twitter.com/ilyabo"><FaTwitter/></a>
-        <a href="https://www.linkedin.com/in/ilyabo/"><FaLinkedin/></a>
-        <a href="https://github.com/ilyabo"><FaGithub/></a>
-      </div>
+      <SocialLinksArea>
+        <a href="https://twitter.com/ilyabo" target="_blank" rel="noopener"><FaTwitter/></a>
+        <a href="https://www.linkedin.com/in/ilyabo/" target="_blank" rel="noopener"><FaLinkedin/></a>
+        <a href="https://github.com/ilyabo" target="_blank" rel="noopener"><FaGithub/></a>
+      </SocialLinksArea>
     </Outer>
   </Headroom>
 
